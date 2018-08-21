71e0a09e631fb5e8e4b23269f391bc10441b5551
SebPud <sebapudelko93@gmail.com>
2018-08-21 14:28:58 +0200

    Add mathemathics

--- src/com/sebapudelko93/Main.java
+++ src/com/sebapudelko93/Main.java
@@ -6,6 +6,9 @@ public class Main {
         System.out.println("Hello Git");
         System.out.println(getName());
         System.out.println(getAge());
+        printMatEquationResults(5, 7);
+        System.out.println(isEven(7));
+        System.out.println(couldDivideBy3and5(31));
     }
     private static String getName() {
         return "Sebastian";
@@ -13,6 +16,19 @@ public class Main {
     private static int getAge() {
         return 24;
     }
+    private static void printMatEquationResults(double a, double b){
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+    private static boolean isEven(int a){
+        return a%2 ==0;
+    }
+    private static boolean couldDivideBy3and5(int number){
+        return number % 15 == 0;
+    }
+
 
 
 
