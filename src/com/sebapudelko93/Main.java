5b628b9cb32d08ddedcf1e0ab3bfd641e429297a
SebPud <sebapudelko93@gmail.com>
2018-08-21 14:12:56 +0200

    Add new methods

--- src/com/sebapudelko93/Main.java
+++ src/com/sebapudelko93/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println(getName());
+        System.out.println(getAge());
     }
+    private static String getName() {
+        return "Sebastian";
+    }
+    private static int getAge() {
+        return 24;
+    }
+
+
+
 }
