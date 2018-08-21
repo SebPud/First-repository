567a4631d59d9b4958229c2aee7026f8cecbd219
SebPud <sebapudelko93@gmail.com>
2018-08-21 14:43:34 +0200

    Next Math

--- src/com/sebapudelko93/Main.java
+++ src/com/sebapudelko93/Main.java
@@ -8,7 +8,10 @@ public class Main {
         System.out.println(getAge());
         printMatEquationResults(5, 7);
         System.out.println(isEven(7));
-        System.out.println(couldDivideBy3and5(31));
+        System.out.println(couldDivideBy3and5(30));
+        System.out.println(pow3(6));
+        System.out.println(square(81));
+        System.out.println(triangle(2,3,4));
     }
     private static String getName() {
         return "Sebastian";
@@ -27,6 +30,18 @@ public class Main {
     }
     private static boolean couldDivideBy3and5(int number){
         return number % 15 == 0;
+    }
+    private static double pow3(double number){
+        return Math.pow(number,3);
+    }
+    private static double square(double number){
+        return Math.sqrt(number);
+    }
+    private static boolean triangle(double a, double b, double c){
+        if (a+b<c) return false;
+        else if (a+c<b) return false;
+        else if (b+c<a) return false;
+        else return true;
     }
 
 
